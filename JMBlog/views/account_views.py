a8079aa4533b887bbb1c65cfe75d63232a7f3938
JiminLeeDev <JiminLeeDev0220@gmail.com>
2021-06-28 23:25:41 +0900

    Login and account_info page added. Login logic added.

--- JMBlog/views/account_views.py
+++ JMBlog/views/account_views.py
@@ -1,8 +1,9 @@
 import flask
+from flask.helpers import url_for
 import JMBlog
 import models
-
 from flask import request, render_template
+from werkzeug.utils import redirect
 
 bp = flask.Blueprint("account", __name__, url_prefix="/account/")
 
@@ -26,4 +27,26 @@ def RegisterConfirm():
     except:
         print("Validation is failed.")
 
-    return render_template("index.html")
+    return redirect(url_for("account.Login"))
+
+
+@bp.route("/login/")
+def Login():
+    return render_template("login.html")
+
+
+@bp.route("/login/confirm", methods=["POST"])
+def LoginConfirm():
+    account_list = JMBlog.db.session.query(models.Account).all()
+    for account in account_list:
+        print(account.id)
+        print(account.id)
+
+        if account.id == request.form.get(
+            "id"
+        ) and account.password == request.form.get("password"):
+            print("2")
+            return render_template("account_info.html", account=account)
+
+    print("3")
+    return redirect(url_for("main.Index"))
